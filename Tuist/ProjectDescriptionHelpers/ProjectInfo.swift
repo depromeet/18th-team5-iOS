6eeafafa4a6f9fffa9313226e7c6f257a0c418b5
Jungwon Lee <dev.jungwon.lee@gmail.com>
2026-04-06 22:55:51 +0900

    🚚 Mod: 앱 이름 Peaktime으로 수정

--- Tuist/ProjectDescriptionHelpers/ProjectInfo.swift
+++ Tuist/ProjectDescriptionHelpers/ProjectInfo.swift
@@ -8,7 +8,7 @@
 import ProjectDescription
 
 public enum ProjectInfo {
-    public static let appName: String = "Orange" // TODO: 추후에 변경 예정 - @정원
+    public static let appName: String = "Peaktime"
     public static let organizationName: String = "Orange"
     public static let destinations: Destinations = [.iPhone]
     public static let deploymentTargets: DeploymentTargets = .iOS("17.0")
