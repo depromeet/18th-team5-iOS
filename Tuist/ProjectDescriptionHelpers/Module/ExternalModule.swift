bb4532e387e7c96b46f920dd9919dcbf99bb4d58
Jungwon Lee <dev.jungwon.lee@gmail.com>
2026-04-11 19:38:28 +0900

    💡 Add: ComposableArchitecture, Dependencies 외부 모듈 추가

--- Tuist/ProjectDescriptionHelpers/Module/ExternalModule.swift
+++ Tuist/ProjectDescriptionHelpers/Module/ExternalModule.swift
@@ -9,10 +9,14 @@ import ProjectDescription
 
 enum ExternalModule {
     case alamofire
+    case composableArchitecture
+    case dependencies
     
     var name: String {
         switch self {
         case .alamofire: "Alamofire"
+        case .composableArchitecture: "ComposableArchitecture"
+        case .dependencies: "Dependencies"
         }
     }
 }
