9c39c0d98bc0634d16e6ec454f19d40097b637ca
minkyo <smk78449@naver.com>
2026-04-25 20:20:51 +0900

    ✨ Feat: CornerRadius 설정

---
+++ Projects/DesignSystem/Sources/CornerRadius.swift
@@ -0,0 +1,19 @@
+//
+//  CornerRadius.swift
+//  DesignSystem
+//
+//  Created by 송민교 on 4/25/26.
+//  Copyright © 2026 Orange. All rights reserved.
+//
+
+import CoreGraphics
+
+public extension CGFloat {
+    static let radius4: CGFloat = 4
+    static let radius8: CGFloat = 8
+    static let radius12: CGFloat = 12
+    static let radius16: CGFloat = 16
+    static let radius20: CGFloat = 20
+    static let radius24: CGFloat = 24
+    static let radius999: CGFloat = 999
+}
