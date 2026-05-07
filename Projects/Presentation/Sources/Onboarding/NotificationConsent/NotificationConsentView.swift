14ff0ca89edacd8eb9984ac68442d586ee5f1930
Jungwon Lee <dev.jungwon.lee@gmail.com>
2026-05-07 20:23:08 +0900

    ✨ Feat: 알림 동의 화면 디자인 반영

--- Projects/Presentation/Sources/Onboarding/NotificationConsent/NotificationConsentView.swift
+++ Projects/Presentation/Sources/Onboarding/NotificationConsent/NotificationConsentView.swift
@@ -19,27 +19,63 @@ public struct NotificationConsentView: View {
 
     public var body: some View {
         VStack(spacing: 0) {
-            VStack(spacing: 56) {
-                Text("절기 알림과 미션을 위해\n알림 허용이 필요해요")
-                    .font(.headline2Semibold)
-                    .foregroundStyle(Color.gray800)
-                    .multilineTextAlignment(.center)
+            VStack(spacing: 48) {
+                VStack(spacing: 12) {
+                    Image.icAlarmClock
+                        .resizable()
+                        .frame(width: 48, height: 48)
+
+                    Text(title)
+                        .font(.headline2Semibold)
+                        .foregroundStyle(Color.gray900)
+                        .multilineTextAlignment(.center)
+                }
 
                 Image.imgNotificationGuide
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
 
-            doneButton
+            VStack(spacing: 16) {
+                nextButton
+                skipButton
+            }
+            .padding(.horizontal, 20)
+            .padding(.top, 8)
+            .padding(.bottom, 20)
         }
+        .background(LinearGradient.onboardingBackground)
     }
 }
 
 private extension NotificationConsentView {
-    var doneButton: some View {
-        BottomButton(title: "네, 확인했어요") {
-            store.send(.agreeButtonTapped)
+    var title: String {
+        """
+        알림을 허용하면, 절기를 놓치지 않고
+        미션에도 쉽게 참여할 수 있어요!
+        """
+    }
+
+    var nextButton: some View {
+        BottomButton(title: "다음") {
+            store.send(.nextButtonTapped)
         }
-        .padding(.horizontal, 20)
-        .padding(.bottom, 16)
+    }
+
+    var skipButton: some View {
+        Button {
+            store.send(.skipButtonTapped)
+        } label: {
+            HStack(spacing: 0) {
+                Text("건너뛰기")
+                    .font(.body1Medium)
+                    .foregroundStyle(Color.gray600)
+
+                Image.icArrowRight
+                    .renderingMode(.template)
+                    .resizable()
+                    .frame(width: 24, height: 24)
+                    .foregroundStyle(Color.gray600)
+            }
+        }
     }
 }
