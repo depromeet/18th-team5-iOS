6c8998535ce86d6ef712338e89ea2cf34505451d
J0onYEong <cjy2134@naver.com>
2026-05-16 12:32:08 +0900

    ✨ Feat: SolarTerm엔티티 구현

---
+++ Projects/Domain/Sources/Entity/Common/Season.swift
@@ -0,0 +1,14 @@
+//
+//  Season.swift
+//  Domain
+//
+//  Created by choijunios on 5/16/26.
+//  Copyright © 2026 Orange. All rights reserved.
+//
+
+public enum Season {
+    case spring
+    case summer
+    case autumn
+    case winter
+}
