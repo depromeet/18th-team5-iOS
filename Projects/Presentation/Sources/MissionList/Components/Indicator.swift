53a91033040267278e997245b9b0f205d4112972
Jungwon Lee <dev.jungwon.lee@gmail.com>
2026-05-25 14:11:57 +0900

    🐛 Fix: stored property로 수정

--- Projects/Presentation/Sources/MissionList/Components/Indicator.swift
+++ Projects/Presentation/Sources/MissionList/Components/Indicator.swift
@@ -15,6 +15,13 @@ struct Indicator: View {
     @State private var lastChangedIndex: Int?
     @State private var latestLocation: CGPoint?
 
+    private let selectedWidth: CGFloat = 20
+    private let selectedHeight: CGFloat = 4
+    private let unselectedWidth: CGFloat = 10
+    private let unselectedHeight: CGFloat = 2
+    private let itemSpacing: CGFloat = 8
+    private let contentPadding: CGFloat = 6
+
     private let totalCount: Int
     private let selectedIndex: Int
     private let mainColor: Color
@@ -58,32 +65,6 @@ struct Indicator: View {
             activationTask?.cancel()
             isEnabled = false
         }
-    }
-}
-
-private extension Indicator {
-    var selectedWidth: CGFloat {
-        20
-    }
-
-    var selectedHeight: CGFloat {
-        4
-    }
-
-    var unselectedWidth: CGFloat {
-        10
-    }
-
-    var unselectedHeight: CGFloat {
-        2
-    }
-
-    var itemSpacing: CGFloat {
-        8
-    }
-
-    var contentPadding: CGFloat {
-        6
     }
 }
 
