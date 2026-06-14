11fa6df1e7e0d593a88c5320a6c6a8e7caf152a3
J0onYEong <cjy2134@naver.com>
2026-06-14 18:28:37 +0900

    🐛 Fix: 카드인덱스 표시안되는 버그 수정

--- Projects/Presentation/Sources/Calendar/Detail/Subviews/CardCountBadge.swift
+++ Projects/Presentation/Sources/Calendar/Detail/Subviews/CardCountBadge.swift
@@ -21,9 +21,8 @@ struct CardCountBadge: View {
             .padding(.horizontal, 12)
             .padding(.vertical, 5)
             .background(.ultraThinMaterial)
-            .background(Color.blackAlpha300.opacity(0.12))
+            .background(Color.blackAlpha300)
             .clipShape(Capsule())
-            .blur(radius: 10)
     }
 }
 
