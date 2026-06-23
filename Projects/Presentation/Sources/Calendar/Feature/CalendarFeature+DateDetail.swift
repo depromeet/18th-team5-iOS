826144a6cf74905db8c740bc40fbc3ff1f641ff8
Jungwon Lee <dev.jungwon.lee@gmail.com>
2026-06-23 19:51:55 +0900

    🐛 Fix: 기록 유무 판단 로직 수정

--- Projects/Presentation/Sources/Calendar/Feature/CalendarFeature+DateDetail.swift
+++ Projects/Presentation/Sources/Calendar/Feature/CalendarFeature+DateDetail.swift
@@ -47,7 +47,7 @@ extension CalendarFeature {
               )?.solarTermInfo.term
         else { return .none }
 
-        let hasRecord = state.dateData(dateModel) != nil
+        let hasRecord = state.dateData(dateModel)?.thumbnailURL != nil
         analyticsClient.logCalendarDateTap(date, hasRecord)
 
         state.detail = .init(date: date, term: term)
